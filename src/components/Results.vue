7bda3a9fb150acfb3c838ffba521470c2bc1cb3f
Ariel Davis <amanichai100@gmail.com>
2023-03-01 21:24:20 -0500

    style

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,9 +1,47 @@
 <template>
   <p>Reaction Time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-    props: ['score']
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "Speedy"
+        } else if (this.score < 400) {
+            this.rank = "Rapid Reflexes"
+        } else {
+            this.rank = "snail pace..."
+        }
+    }
 }
 </script>
+
+<style>
+.rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
+</style>
